f51282a09bf97cc6c3d7bff6e5c040e474821776
lingdocs <71590811+lingdocs@users.noreply.github.com>
2021-09-17 16:39:10 -0400

    ability to publish types

---
+++ dist/website/src/types/account-types.d.ts
@@ -0,0 +1,98 @@
+export declare type Hash = string & {
+    __brand: "Hashed String";
+};
+export declare type UUID = string & {
+    __brand: "Random Unique UID";
+};
+export declare type TimeStamp = number & {
+    __brand: "UNIX Timestamp in milliseconds";
+};
+export declare type UserDbPassword = string & {
+    __brand: "password for an individual user couchdb";
+};
+export declare type WordlistDbName = string & {
+    __brand: "name for an individual user couchdb";
+};
+export declare type URLToken = string & {
+    __brand: "Base 64 URL Token";
+};
+export declare type EmailVerified = true | Hash | false;
+export declare type ActionComplete = {
+    ok: true;
+    message: string;
+};
+export declare type ActionError = {
+    ok: false;
+    error: string;
+};
+export declare type APIResponse = ActionComplete | ActionError | {
+    ok: true;
+    user: LingdocsUser;
+};
+export declare type WoutRJ<T> = Omit<T, "_raw" | "_json">;
+export declare type GoogleProfile = WoutRJ<import("passport-google-oauth").Profile> & {
+    refreshToken: string;
+    accessToken: string;
+};
+export declare type GitHubProfile = WoutRJ<import("passport-github2").Profile> & {
+    accessToken: string;
+};
+export declare type TwitterProfile = WoutRJ<import("passport-twitter").Profile> & {
+    token: string;
+    tokenSecret: string;
+};
+export declare type ProviderProfile = GoogleProfile | GitHubProfile | TwitterProfile;
+export declare type UserLevel = "basic" | "student" | "editor";
+export declare type UserTextOptions = Omit<import("@lingdocs/pashto-inflector").Types.TextOptions, "pTextSize">;
+export declare type UserTextOptionsRecord = {
+    lastModified: TimeStamp;
+    userTextOptions: UserTextOptions;
+};
+export declare type LingdocsUser = {
+    userId: UUID;
+    admin?: boolean;
+    password?: Hash;
+    name: string;
+    email?: string;
+    emailVerified: EmailVerified;
+    github?: GitHubProfile;
+    google?: GoogleProfile;
+    twitter?: TwitterProfile;
+    passwordReset?: {
+        tokenHash: Hash;
+        requestedOn: TimeStamp;
+    };
+    upgradeToStudentRequest?: "waiting" | "denied";
+    tests: [];
+    lastLogin: TimeStamp;
+    lastActive: TimeStamp;
+    userTextOptionsRecord: undefined | UserTextOptionsRecord;
+} & ({
+    level: "basic";
+} | {
+    level: "student" | "editor";
+    couchDbPassword: UserDbPassword;
+    wordlistDbName: WordlistDbName;
+}) & import("nano").MaybeDocument;
+export declare type CouchDbAuthUser = {
+    type: "user";
+    name: UUID;
+    password: UserDbPassword;
+    roles: [];
+} & import("nano").MaybeDocument;
+export declare type UpgradeUserResponse = {
+    ok: false;
+    error: "incorrect password";
+} | {
+    ok: true;
+    message: "user already upgraded" | "user upgraded to student";
+    user: LingdocsUser;
+};
+export declare type UpdateUserTextOptionsRecordBody = {
+    userTextOptionsRecord: UserTextOptionsRecord;
+};
+export declare type UpdateUserTextOptionsRecordResponse = {
+    ok: true;
+    message: "updated userTextOptionsRecord";
+    user: LingdocsUser;
+};
---
+++ dist/website/src/types/functions-types.d.ts
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2021 lingdocs.com
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { Types as T } from "@lingdocs/pashto-inflector";
+import * as AT from "./account-types";
+export declare type FunctionResponse = PublishDictionaryResponse | SubmissionsResponse | FunctionError;
+export declare type FunctionError = {
+    ok: false;
+    error: string;
+};
+export declare type PublishDictionaryResponse = {
+    ok: true;
+    info: T.DictionaryInfo;
+} | {
+    ok: false;
+    errors: T.DictionaryEntryError[];
+};
+export declare type Submission = Edit | ReviewTask;
+export declare type Edit = EntryEdit | NewEntry | EntryDeletion;
+export declare type SubmissionBase = {
+    _id: string;
+    sTs: number;
+    user: {
+        userId: AT.UUID;
+        name: string;
+        email: string;
+    };
+};
+export declare type ReviewTask = Issue | EditSuggestion | EntrySuggestion;
+export declare type EntryEdit = SubmissionBase & {
+    type: "entry edit";
+    entry: T.DictionaryEntry;
+};
+export declare type EntryDeletion = SubmissionBase & {
+    type: "entry deletion";
+    ts: number;
+};
+export declare type NewEntry = SubmissionBase & {
+    type: "new entry";
+    entry: T.DictionaryEntry;
+};
+export declare type Issue = SubmissionBase & {
+    type: "issue";
+    content: string;
+};
+export declare type EditSuggestion = SubmissionBase & {
+    type: "edit suggestion";
+    entry: T.DictionaryEntry;
+    comment: string;
+};
+export declare type EntrySuggestion = SubmissionBase & {
+    type: "entry suggestion";
+    entry: T.DictionaryEntry;
+    comment: string;
+};
+export declare type SubmissionsRequest = Submission[];
+export declare type SubmissionsResponse = {
+    ok: true;
+    message: string;
+    submissions: Submission[];
+};
