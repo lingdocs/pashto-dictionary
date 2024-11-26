2efa3086f207d90e8097385eb518b03ca7ddb740
adueck <adueck@users.noreply.github.com>
2024-11-26 17:04:26 +0500

    m

--- new-functions/src/index.ts
+++ new-functions/src/index.ts
@@ -1,20 +1,28 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { createMiddleware } from "hono/factory";
+import type { LingdocsUser } from "../../website/src/types/account-types";
 
 const app = new Hono();
 app.use(cors());
 
-app.use(async (c, next) => {
+const authMiddleware = createMiddleware<{
+  Variables: {
+    user: LingdocsUser;
+  };
+}>(async (c, next) => {
   const cookie = c.req.header("Cookie") || "";
   const r = await fetch("https://account.lingdocs.com/api/user", {
     headers: { cookie },
   });
-  const res = await r.json();
+  const res = (await r.json()) as { ok: boolean; user: LingdocsUser };
   if (res.ok) {
     c.set("user", res.user);
   }
   await next();
 });
+
+app.use(authMiddleware);
 
 app.get("/", (c) => {
   // c.env.LINGDOCS_COUCHDB
