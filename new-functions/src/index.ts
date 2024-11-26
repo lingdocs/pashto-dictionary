a3c5d1b588f6094c3dbf8f92ab16cd32de06ca1a
adueck <adueck@users.noreply.github.com>
2024-11-26 16:47:05 +0500

    m

--- new-functions/src/index.ts
+++ new-functions/src/index.ts
@@ -7,4 +7,9 @@ app.get("/", (c) => {
   return c.text("Hi from hono updated");
 });
 
+app.get("/wa", (c) => {
+  // c.env.LINGDOCS_COUCHDB
+  return c.text("Hi other route");
+});
+
 export default app;
