d98169b2e21327c57ed1655a4b3359430bbbe00b
adueck <adueck@users.noreply.github.com>
2025-08-28 11:32:59 +0500

    fix new redis library issue

--- account/src/middleware/setup-session.ts
+++ account/src/middleware/setup-session.ts
@@ -1,32 +1,39 @@
 import session from "express-session";
 import { Express } from "express";
-import redis from "redis";
+import { createClient } from "redis";
 import inProd from "../lib/inProd";
 import env from "../lib/env-vars";
+import { RedisStore } from "connect-redis";
 
 const FileStore = !inProd ? require("session-file-store")(session) : undefined;
 
-const RedisStore = require("connect-redis")(session);
+const redisClient = inProd ? createClient() : undefined;
+redisClient?.connect().catch(console.error);
+
+const redisStore = inProd
+  ? new RedisStore({
+      client: redisClient,
+    })
+  : undefined;
 
 function setupSession(app: Express) {
-    app.use(
-        session({
-            secret: env.cookieSecret,
-            name: "__session",
-            resave: true,
-            saveUninitialized: false,
-            proxy: inProd,
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 7 * 30 * 6,
-                secure: inProd,
-                domain: inProd ? "lingdocs.com" : undefined,
-                httpOnly: true,
-            },
-            store: inProd
-                ? new RedisStore({ client: redis.createClient() })
-                : new FileStore({}),
-        })
-    );
+  app.use(
+    session({
+      secret: env.cookieSecret,
+      name: "__session",
+      resave: true,
+      saveUninitialized: false,
+      proxy: inProd,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 30 * 6,
+        secure: inProd,
+        domain: inProd ? "lingdocs.com" : undefined,
+        httpOnly: true,
+      },
+      store: inProd ? redisStore : new FileStore({}),
+    }),
+  );
 }
 
 export default setupSession;
+
